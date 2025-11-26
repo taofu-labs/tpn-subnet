4e3f055e98c44bb9c2ae357996a90151197f379f
Mentor <mentor@palokaj.co>
2025-11-26 13:21:00 +0100

    ✨ Make update script test for open ports

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -18,7 +18,6 @@ FORCE_RESTART=true
 stash_created=false
 stash_popped=false
 
-
 # RAM settings
 QUARTER_OF_FREE_RAM=$(($(free -m | awk 'NR==2{print $2}') * 3 / 4))
 export CONTAINER_MAX_PROCESS_RAM_MB=${CONTAINER_MAX_PROCESS_RAM_MB:-$QUARTER_OF_FREE_RAM}
@@ -35,6 +34,41 @@ print_help() {
   echo "  --help                         Show this help message and exit"
   exit 0
 }
+
+# --------------------
+# Helpers
+# --------------------
+
+green() {
+  if [ $# -gt 0 ]; then
+    printf '\033[0;32m%s\033[0m\n' "$*"
+  else
+    while IFS= read -r line; do
+      printf '\033[0;32m%s\033[0m\n' "$line"
+    done
+  fi
+}
+
+red() {
+  if [ $# -gt 0 ]; then
+    printf '\033[0;31m%s\033[0m\n' "$*"
+  else
+    while IFS= read -r line; do
+      printf '\033[0;31m%s\033[0m\n' "$line"
+    done
+  fi
+}
+
+grey() {
+  if [ $# -gt 0 ]; then
+    printf '\033[0;90m%s\033[0m\n' "$*"
+  else
+    while IFS= read -r line; do
+      printf '\033[0;90m%s\033[0m\n' "$line"
+    done
+  fi
+}
+
 
 # Parse command-line arguments
 for arg in "$@"; do
@@ -68,7 +102,7 @@ done
 
 # Check for TPN repository
 if [ ! -d "$TPN_DIR" ]; then
-    echo "TPN repository not found at $TPN_DIR. Please clone it first."
+    red "TPN repository not found at $TPN_DIR. Please clone it first."
     exit 1
 fi
 
@@ -78,22 +112,19 @@ if [ -f "$TPN_DIR/federated-container/.env" ]; then
     . "$TPN_DIR/federated-container/.env"
     set +a
 else
-    echo "Warning: .env file not found in $TPN_DIR/federated-container. Exiting."
+    red "Warning: .env file not found in $TPN_DIR/federated-container. Exiting."
     exit 1
 fi
 
 # Run mode settings
 RUN_MODE="${RUN_MODE:-}"
 if [ "$RUN_MODE" != "worker" ] && [ "$RUN_MODE" != "miner" ] && [ "$RUN_MODE" != "validator" ]; then
-    echo "RUN_MODE must be one of worker, miner, or validator. Current value: '$RUN_MODE'." >&2
+    red "RUN_MODE must be one of worker, miner, or validator. Current value: '$RUN_MODE'." >&2
     exit 1
 fi
 
 # Set default pm2 process name if not provided
 PM2_PROCESS_NAME=${PM2_PROCESS_NAME:-tpn_$RUN_MODE}
-
-
-
 
 # Get pm2 binary, default to $HOME/.npm-global/bin/pm2
 PM2_BIN_PATH=$(command -v pm2 || echo "$HOME/.npm-global/bin/pm2")
@@ -115,7 +146,6 @@ if [ -n "${SWAG_DOMAIN_NAME:-}" ] && [ "${SWAG_DISABLE_SSL:-false}" != "true" ] 
     DOCKER_CMD+=(--profile proxy)
 fi
 
-
 # If run mode is worker, add --profile worker
 if [ "$RUN_MODE" = "worker" ]; then
     DOCKER_CMD+=(--profile worker)
@@ -133,19 +163,30 @@ printf '\n'
 restart_command="0 * * * * bash $TPN_DIR/scripts/update_node.sh --force_restart=false"
 
 if [ "$ENABLE_AUTOUPDATE" = "true" ]; then
+
     # Dump crontab, fallback to empty if none exists
     existing_cron=$(crontab -l 2>/dev/null || true)
     
     # Check if restart_command already exists
     if ! printf '%s\n' "$existing_cron" | grep -Fxq "$restart_command"; then
+
         # Remove any old node update entries
         new_cron=$(printf "%s" "$existing_cron" | grep -v "scripts/update_node.sh" || true)
 
+        # Append the new cron job
+        new_cron=$(printf "%s\n%s" "$new_cron" "$restart_command")
+
         # Add the correct restart_command
         printf "%s\n%s\n" "$new_cron" "$restart_command" | crontab -
+        grey "Tab is now up to date"
+
+    else
+
+        grey "Tab was already up to date, no changes made."
+
     fi
 else
-    echo "Autoupdate disabled, skipping crontab check."
+    grey "Autoupdate disabled, skipping crontab check."
 fi
 
 # Stash local changes before pulling
@@ -156,7 +197,7 @@ if git -C "$TPN_DIR" stash push -m "Stash before update on $(date)" >/dev/null 2
     if [ "$pre_stash_ref" != "$post_stash_ref" ]; then
         stash_created=true
     else
-        echo "No changes to stash."
+        grey "No changes to stash."
     fi
 else
     echo "Failed to stash changes, continuing anyway."
@@ -189,9 +230,9 @@ if [ "$FORCE_RESTART" = "true" ]; then
 fi
 
 # Pull the latest docker images
-echo "Pulling latest docker images..."
+grey "Pulling latest docker images..."
 "${DOCKER_CMD[@]}" pull -q
-echo "Latest docker images pulled."
+grey "Latest docker images pulled."
 
 # Restart the node docker container if needed
 if [ "$REPO_UP_TO_DATE" -eq 0 ]; then
@@ -202,7 +243,7 @@ if [ "$REPO_UP_TO_DATE" -eq 0 ]; then
     echo "Pruning unused networks..."
     docker network prune -f || echo "Failed to prune unused networks."
 else
-    echo "No changes in the repository, no need to force restart docker."
+    grey "No changes in the repository, no need to force restart docker."
 fi
 
 # Bring node back up
@@ -210,14 +251,65 @@ fi
 
 # Restart the pm2 process if needed, only for non worker nodes
 if [ "$RUN_MODE" != "worker" ]; then
+
+    # Restart neuron process if repo has changes
     if [ "$REPO_UP_TO_DATE" -eq 0 ]; then
+
+        # Update python dependencies
+        echo "Repository has changes, updating python dependencies..."
+        cd ~/tpn-subnet
+        python3 -m venv venv
+        source venv/bin/activate
+        TPN_CACHE="$HOME/.tpn_cache"
+        mkdir -p $TPN_CACHE
+        export TMPDIR=$TPN_CACHE
+        export WANDB_CACHE_DIR=$TPN_CACHE
+        pip3 install -r requirements.txt
+    
         echo "Repository has changes, restarting pm2 process $PM2_PROCESS_NAME..."
         $PM2_BIN_PATH restart "$PM2_PROCESS_NAME"
+
     else
-        echo "No changes in the repository, skipping pm2 restart."
+        grey "No changes in the repository, skipping pm2 restart."
     fi
 else
-    echo "pm2 not relevant in worker mode, skipping pm2 restart."
+    grey "pm2 not relevant in worker mode, skipping pm2 restart."
 fi
 
-echo -e "\n✅ Update completed successfully"
+# Do a sanity check on open ports
+SERVER_PUBLIC_PORT="${SERVER_PUBLIC_PORT:-3000}"
+DANTE_PORT="${DANTE_PORT:-1080}"
+WIREGUARD_SERVERPORT="${WIREGUARD_SERVERPORT:-51820}"
+NETCAT_AVAILABLE=$(command -v nc || command -v netcat || echo "")
+if [ -n "$NETCAT_AVAILABLE" ]; then
+    echo "Performing sanity check on open ports..."
+
+    # For every run mode, the public port (tcp) and wireguard (udp) port are suggested to be open (but in many cases docker will bypass localised firewall rules)
+    if nc -zv localhost "$SERVER_PUBLIC_PORT" >/dev/null 2>&1; then
+        green "✅ Public port $SERVER_PUBLIC_PORT/tcp is open."
+    else 
+        echo "🚨 Warning: Public port $SERVER_PUBLIC_PORT/tcp is not open. It is recommended to open it in your firewall for proper node functionality."
+    fi
+    if nc -zvu localhost "$WIREGUARD_SERVERPORT" >/dev/null 2>&1; then
+        green "✅ Wireguard port $WIREGUARD_SERVERPORT/udp is open."
+    else 
+        echo "🚨 Warning: Wireguard port $WIREGUARD_SERVERPORT/udp is not open. It is recommended to open it in your firewall for proper node functionality."
+    fi
+
+    # For worker mode, dante port MUST be open because the container does not bypass local firewall rules
+    if [ "$RUN_MODE" = "worker" ]; then
+        if nc -zv localhost "$DANTE_PORT" >/dev/null 2>&1; then
+            green "✅ Dante port $DANTE_PORT/tcp is open."
+        else 
+            red "🚨 Error: Dante port $DANTE_PORT/tcp is not open. Please open it in your firewall for proper worker functionality."
+        fi
+    fi
+
+
+else
+    echo "⚠️ Netcat not available, skipping port sanity checks. Please install netcat-openbsd package to enable this feature."
+fi
+
+
+
+green -e "\n✅ Update completed successfully"
